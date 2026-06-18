a081838084d7bfc3512a2e8d09d6c32998455044
Aguilam <pexfex@mail.ru>
2026-06-18 23:46:16 +0400

    feat(main): add base routes drafts

--- main.go
+++ main.go
@@ -9,5 +9,46 @@ func main() {
       "message": "pong",
     })
   })
+  router.GET("items",func(ctx *gin.Context) {
+    ctx.JSON(200,gin.H{
+      "items": "items",
+    })
+  })
+  router.GET("type",func(ctx *gin.Context) {
+    ctx.JSON(200,gin.H{
+      "type": "type",
+    })
+  })
+  router.GET("barrel",func(ctx *gin.Context) {
+    ctx.JSON(200,gin.H{
+      "barrel": "barrel",
+    })
+  })
+  router.GET("seller",func(ctx *gin.Context) {
+    ctx.JSON(200,gin.H{
+      "seller": "seller",
+    })
+  })
+
+  router.POST("item",func(ctx *gin.Context) {
+    ctx.JSON(200,gin.H{
+      "item": "item",
+    })
+  })
+  router.POST("type",func(ctx *gin.Context) {
+    ctx.JSON(200,gin.H{
+      "type": "type",
+    })
+  })
+  router.POST("barrel",func(ctx *gin.Context) {
+    ctx.JSON(200,gin.H{
+      "barrel": "barrel",
+    })
+  })
+  router.POST("seller",func(ctx *gin.Context) {
+    ctx.JSON(200,gin.H{
+      "seller": "seller",
+    })
+  })
   router.Run()
 }
