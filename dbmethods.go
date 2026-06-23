47564e54f61b556ec1a13c57808bb5029e43f2d2
Aguilam <pexfex@mail.ru>
2026-06-23 16:23:54 +0400

    feat(db): add db methods for get data

---
+++ dbmethods.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"fmt"
+
+	"zombiezen.com/go/sqlite"
+	"zombiezen.com/go/sqlite/sqlitex"
+)
+func getBarrelHistory(conn *sqlite.Conn,x int,y int, z int) {
+	return
+}
+
+func getTypes(conn *sqlite.Conn)([]AllTypes,error){
+	query := `
+		SELECT COUNT(item.id),type.id,type.name 
+		FROM item_type as type 
+		LEFT JOIN item ON type_id = type.id
+		GROUP BY type.id;`
+	var allTypes []AllTypes
+	err := sqlitex.Execute(conn,query,&sqlitex.ExecOptions{
+		ResultFunc: func(stmt *sqlite.Stmt) error {
+			item := AllTypes{
+				Count : stmt.ColumnInt32(0),
+				Id : stmt.ColumnInt32(1),
+				Name: stmt.ColumnText(2),
+			}
+			allTypes = append(allTypes, item)
+			return nil
+		},
+	})
+	if err != nil {
+		return nil,fmt.Errorf("Get all types error: %w",err)
+	}
+	return allTypes,nil
+}
+
+func getItemsByType(conn *sqlite.Conn, typeId int) ([]Item,error){
+	query := `
+		SELECT id, name, description, normalized_name 
+		FROM item
+		WHERE item.type_id = ?;`
+	var items []Item
+	err := sqlitex.Execute(conn,query,&sqlitex.ExecOptions{
+		ResultFunc: func(stmt *sqlite.Stmt) error {
+			var desc *string
+
+    		if stmt.ColumnType(2) != sqlite.TypeNull {
+    		    text := stmt.ColumnText(2)
+    		    desc = &text 
+    		}
+			item := Item{
+				Id: stmt.ColumnInt32(0),
+				Name: stmt.ColumnText(1),
+				Description: desc,
+				NormalizedName: stmt.ColumnText(3),
+			}
+			items = append(items,item)
+			return nil
+		},
+		Args: []any{typeId},
+	})
+	if err != nil {
+		return nil,fmt.Errorf("Get items by type error: %w",err)
+	}
+	return items,nil
+}
+
+func getBarrelsBySellerId(conn *sqlite.Conn,sellerId int32) ([]BarrelItem, error){
+	query := `
+		SELECT 
+			b_item.id, b_item.item_id, b_item.barrel_id, b_item.seller_id, b_item.quantity, 
+    		b_item.price, b_item.benefit_ratio, b_item.record_date,
+			item.id, item.name, item.description, item.normalized_name, item.type_id
+		FROM barrel_item as b_item
+		JOIN seller ON b_item.seller_id = seller.id
+		JOIN item ON b_item.item_id = item.id
+		WHERE seller.id = ?;`
+	var sellerBarrels []BarrelItem
+	err := sqlitex.Execute(conn,query,&sqlitex.ExecOptions{
+		ResultFunc: func(stmt *sqlite.Stmt) error {
+			var desc *string
+
+    		if stmt.ColumnType(10) != sqlite.TypeNull {
+    		    text := stmt.ColumnText(10)
+    		    desc = &text 
+    		}
+			item := BarrelItem{
+				Id: stmt.ColumnInt32(0),
+				Item: Item{
+					Id: stmt.ColumnInt32(8),
+					Name: stmt.ColumnText(9),
+					Description: desc,
+					NormalizedName: stmt.ColumnText(11),
+				},
+				Quantity: stmt.ColumnInt32(4),
+				Price: stmt.ColumnInt32(5),
+				BenefitRatio: float32(stmt.ColumnFloat(6)),
+				RecordDate: stmt.ColumnText(7),
+			}
+			sellerBarrels = append(sellerBarrels, item)
+			return nil
+		},
+		Args: []any{sellerId},
+	})
+	if err != nil {
+		return nil,fmt.Errorf("Get barrel by seller error: %w",err)
+	}
+	return sellerBarrels,nil
+}
--- dto.go
+++ dto.go
@@ -7,13 +7,35 @@ type NewBarrelPost struct {
 	Message string `json:"message" binding:"required"`
 }
 
+type ItemInBarrelPost struct {
+	Name     string         `json:"name" binding:"required"`
+	Quantity string         `json:"quantity" binding:"required"`
+	Items    []ItemInBarrel `json:"items" binding:"required"`
+}
+
 type ItemInBarrel struct {
 	Name     string `json:"name" binding:"required"`
 	Quantity string `json:"quantity" binding:"required"`
 }
 
-type ItemInBarrelPost struct {
-	Name     string         `json:"name" binding:"required"`
-	Quantity string         `json:"quantity" binding:"required"`
-	Items    []ItemInBarrel `json:"items" binding:"required"`
+type AllTypes struct {
+	Id    int32
+	Name  string
+	Count int32
+}
+
+type Item struct {
+	Id             int32
+	Name           string
+	Description    *string
+	NormalizedName string
+}
+
+type BarrelItem struct {
+	Id           int32
+	Item         Item
+	Quantity     int32
+	Price        int32
+	BenefitRatio float32
+	RecordDate   string
 }
